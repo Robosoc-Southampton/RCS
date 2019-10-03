6c0d350c0b1cdd7d40047e03514f6a88924a8dd0
exerro <benedict.allen2514@gmail.com>
2019-10-03 18:02:07 +0100

    Add BluetoothHandle class for wrapping bluetooth-console.py

--- src/bluetooth/kotlin/main.kt
+++ src/bluetooth/kotlin/main.kt
@@ -1,41 +1,10 @@
-import java.lang.Exception
 
 fun main() {
-    val rt = Runtime.getRuntime()
-    val command = arrayOf(
-            "python",
-            "src/bluetooth/python/bluetooth-console.py",
-            "--addr",
-            "98:D3:32:11:17:5F"
-    )
-    val proc = rt.exec(command)
+    val handle = BluetoothHandle("98:D3:32:11:17:5F")
 
-    proc.inputStream.read()
-
-    try {
-        println("Here")
-
-        proc.outputStream.write(byteArrayOf(128.toByte()))
-        proc.outputStream.flush()
-
-        Thread.sleep(1000)
-
-        proc.outputStream.write(byteArrayOf(0.toByte()))
-        proc.outputStream.flush()
-
-        Thread.sleep(1000)
-
-        proc.outputStream.close()
-    }
-    catch (e: Exception) {
-        e.printStackTrace()
-    }
-
-    println("in")
-    proc.destroy()
-    println("out")
-    println("Exit value: ${proc.exitValue()}")
-
-    println(String(proc.inputStream.readAllBytes()))
-    println(String(proc.errorStream.readAllBytes()))
+    handle.send(byteArrayOf(128.toByte()))
+    Thread.sleep(1000)
+    handle.send(byteArrayOf(0.toByte()))
+    Thread.sleep(1000)
+    handle.close()
 }
